6a5c97ee971f61586709f74e90f1d57d2a89a5e5
tkachuk-labs <tkachuk.labs@gmail.com>
2020-12-25 15:22:38 +0200

    lazy config installation

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-DOTFILES_DIR="$(dirname "$(readlink -f "$0")")"
+DOTFILES_SOURCE="$(dirname "$(readlink -f "$0")")"
 
 if [[ $UID == 0 ]]; then
   echo "==> Please don't run this script with sudo!"
@@ -16,18 +16,30 @@ sudo apt-get update -y
   echo "==> Installing i3wm ..."
   sudo apt-get install -y i3 unclutter-xfixes
   echo "==> Installing i3wm-config ..."
-  mkdir -p ~/.config/i3/
-  cp "$DOTFILES_DIR/i3wm-config" ~/.config/i3/config
+  mkdir -p ~/.config/i3
+  DOTFILES_TARGET=~/.config/i3/config
+  if [ -f "$DOTFILES_TARGET" ]; then
+    echo "==> File $DOTFILES_TARGET already exists, skipping!"
+  else
+    echo "==> Installing i3wm-config into $DOTFILES_TARGET ..."
+    cp "$DOTFILES_SOURCE/i3wm-config" "$DOTFILES_TARGET"
+  fi
 )
 
 (
   echo "==> Installing vim ..."
   sudo apt-get install -y git vim nodejs
-  echo "==> Installing vim-ide ..."
-  git clone \
-    https://github.com/tim2CF/ultimate-haskell-ide.git \
-    ~/.vim_runtime
-  sh ~/.vim_runtime/install_awesome_vimrc.sh
+  echo "==> Installing vim-config ..."
+  DOTFILES_TARGET=~/.vim_runtime
+  if [ -d "$DOTFILES_TARGET" ]; then
+    echo "==> Directory $DOTFILES_TARGET already exists, skipping!"
+  else
+    echo "==> Installing vim-config into $DOTFILES_TARGET ..."
+    git clone \
+      https://github.com/tim2CF/ultimate-haskell-ide.git \
+      "$DOTFILES_TARGET"
+    sh "$DOTFILES_TARGET/install_awesome_vimrc.sh"
+  fi
 )
 
 (
