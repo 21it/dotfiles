c0b9605945530e44f1e0b96aa3999c1828b07e75
tkachuk-labs <tkachuk.labs@gmail.com>
2020-12-30 23:04:59 +0200

    nix + some utils

--- install.sh
+++ install.sh
@@ -42,21 +42,56 @@ sudo apt-get update -y
   fi
 )
 
-(
-  echo "==> Installing termite-deps ..."
-  sudo apt-get install -y build-essential \
-    git \
-    g++ \
-    libgtk-3-dev \
-    gtk-doc-tools \
-    gnutls-bin \
-    valac \
-    intltool \
-    libpcre2-dev \
-    libglib3.0-cil-dev \
-    libgnutls28-dev \
-    libgirepository1.0-dev \
-    libxml2-utils \
-    gperf \
-    neofetch
-)
+if ! command -v nix &> /dev/null
+then
+  (
+    echo "==> Installing nix ..."
+    cd ~/Downloads
+    sudo apt-get install -y gnupg2
+    curl -o install-nix-2.3.10 \
+      https://releases.nixos.org/nix/nix-2.3.10/install
+    curl -o install-nix-2.3.10.asc \
+      https://releases.nixos.org/nix/nix-2.3.10/install.asc
+    gpg2 \
+      --keyserver hkps://keyserver.ubuntu.com \
+      --recv-keys B541D55301270E0BCF15CA5D8170B4726D7198DE
+    gpg2 --verify ./install-nix-2.3.10.asc
+    sh ./install-nix-2.3.10
+  )
+else
+  echo "==> Nix is already installed, skipping!"
+fi
+
+NIX_CHANNELS="$(nix-channel --list)"
+EXPECTED_NIX_CHANNELS="nixpkgs https://nixos.org/channels/nixos-20.09"
+if [ "$NIX_CHANNELS" != "$EXPECTED_NIX_CHANNELS" ]; then
+  echo "==> Got '$NIX_CHANNELS' but expected '$EXPECTED_NIX_CHANNELS'"
+  echo "==> Installing nixpkgs ..."
+  nix-channel --add https://nixos.org/channels/nixos-20.09 nixpkgs
+  nix-channel --update
+else
+  echo "==> Nix channels are already installed, skipping!"
+fi
+
+strict_install () {
+  echo "==> Installing $1 ..."
+  if [ -z "$2" ]; then
+    nix-env -iP "$1"
+  else
+    eval "$2"
+  fi
+}
+
+lazy_install () {
+  # 1st arg = executable name (required)
+  # 2nd arg = pkg name (optional)
+  # 3rd arg = installation command (optional)
+  local PKG="$([ -z "$2" ] && echo "$1" || echo "$2")"
+  command -v "$1" > /dev/null && \
+    echo "==> $PKG is already installed, skipping!" || \
+    strict_install "$PKG" "$3"
+}
+
+for X in "castget" "irssi" "termite"; do
+  lazy_install $X
+done
