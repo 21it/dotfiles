2281977c8ce9946f8afe96ba4cfa7f6f14686999
tkachuk-labs <tkachuk.labs@gmail.com>
2020-12-31 17:03:29 +0200

    install refactoring

--- install.sh
+++ install.sh
@@ -2,39 +2,77 @@
 
 set -e
 
-DOTFILES_SOURCE="$(dirname "$(readlink -f "$0")")"
+DOTFILES_SOURCE_DIR="$(dirname "$(readlink -f "$0")")"
+
+log () {
+  echo "$1 ==> $2"
+}
+
+log_error () {
+  log "error" "$1"
+}
+
+log_updating () {
+  log "updating" "$1"
+}
+
+log_already_exists () {
+  log "already exists" "$1"
+}
+
+log_creating () {
+  log "creating" "$1"
+}
+
+log_installing () {
+  log "installing" "$1"
+}
+
+log_already_installed () {
+  log "already installed" "$1"
+}
 
 if [[ $UID == 0 ]]; then
-  echo "==> Please don't run this script with sudo!"
+  log_error "please don't run this script with sudo!"
   exit 1
 fi
 
-echo "==> Updating apt-get ..."
+log_updating "apt-get"
 sudo apt-get update -y
 
-(
-  echo "==> Installing i3wm ..."
-  sudo apt-get install -y i3 unclutter-xfixes
-  echo "==> Installing i3wm-config ..."
-  mkdir -p ~/.config/i3
-  DOTFILES_TARGET=~/.config/i3/config
-  if [ -f "$DOTFILES_TARGET" ]; then
-    echo "==> File $DOTFILES_TARGET already exists, skipping!"
+lazy_copy () {
+  local DOTFILES_SOURCE="$DOTFILES_SOURCE_DIR/$1"
+  local DOTFILES_TARGET="$2"
+  local DOTFILES_TARGET_DIR="$(dirname "$(readlink -f "$2")")"
+  if [ -d "$DOTFILES_TARGET_DIR" ]; then
+    log_already_exists "$DOTFILES_TARGET_DIR"
   else
-    echo "==> Installing i3wm-config into $DOTFILES_TARGET ..."
-    cp "$DOTFILES_SOURCE/i3wm-config" "$DOTFILES_TARGET"
+    log_creating "$DOTFILES_TARGET_DIR"
+    mkdir -p "$DOTFILES_TARGET_DIR"
   fi
+  if [ -f "$DOTFILES_TARGET" ]; then
+    log_already_exists "$DOTFILES_TARGET"
+  else
+    log_creating "$DOTFILES_TARGET"
+    cp "$DOTFILES_SOURCE" "$DOTFILES_TARGET"
+  fi
+}
+
+(
+  log_installing "i3wm"
+  sudo apt-get install -y i3 unclutter-xfixes
+  lazy_copy i3wm-config ~/.config/i3/config
 )
 
 (
-  echo "==> Installing vim ..."
+  log_installing "vim"
   sudo apt-get install -y git vim nodejs
-  echo "==> Installing vim-config ..."
+  log_installing "vim_runtime"
   DOTFILES_TARGET=~/.vim_runtime
   if [ -d "$DOTFILES_TARGET" ]; then
-    echo "==> Directory $DOTFILES_TARGET already exists, skipping!"
+    log_already_exists "$DOTFILES_TARGET"
   else
-    echo "==> Installing vim-config into $DOTFILES_TARGET ..."
+    log_installing "$DOTFILES_TARGET"
     git clone \
       https://github.com/tim2CF/ultimate-haskell-ide.git \
       "$DOTFILES_TARGET"
@@ -45,7 +83,7 @@ sudo apt-get update -y
 if ! command -v nix &> /dev/null
 then
   (
-    echo "==> Installing nix ..."
+    log_installing "nix"
     cd ~/Downloads
     sudo apt-get install -y gnupg2
     curl -o install-nix-2.3.10 \
@@ -59,22 +97,22 @@ then
     sh ./install-nix-2.3.10
   )
 else
-  echo "==> Nix is already installed, skipping!"
+  log_already_installed "nix"
 fi
 
 NIX_CHANNELS="$(nix-channel --list)"
 EXPECTED_NIX_CHANNELS="nixpkgs https://nixos.org/channels/nixos-20.09"
 if [ "$NIX_CHANNELS" != "$EXPECTED_NIX_CHANNELS" ]; then
-  echo "==> Got '$NIX_CHANNELS' but expected '$EXPECTED_NIX_CHANNELS'"
-  echo "==> Installing nixpkgs ..."
+  log_error "got '$NIX_CHANNELS' instead of '$EXPECTED_NIX_CHANNELS'"
+  log_installing "nixpkgs"
   nix-channel --add https://nixos.org/channels/nixos-20.09 nixpkgs
   nix-channel --update
 else
-  echo "==> Nix channels are already installed, skipping!"
+  log_already_installed "nixpkgs"
 fi
 
 strict_install () {
-  echo "==> Installing $1 ..."
+  log_installing "$1"
   if [ -z "$2" ]; then
     nix-env -iP "$1"
   else
@@ -88,7 +126,7 @@ lazy_install () {
   # 3rd arg = installation command (optional)
   local PKG="$([ -z "$2" ] && echo "$1" || echo "$2")"
   command -v "$1" > /dev/null && \
-    echo "==> $PKG is already installed, skipping!" || \
+    log_already_installed "$PKG" || \
     strict_install "$PKG" "$3"
 }
 
