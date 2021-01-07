5d78ff05e49de9fbe74cde005f56476cee25bb2c
tkachuk-labs <tkachuk.labs@gmail.com>
2021-01-07 13:41:19 +0200

    install elixir

--- install.sh
+++ install.sh
@@ -130,6 +130,6 @@ lazy_install () {
     strict_install "$PKG" "$3"
 }
 
-for X in "castget" "irssi" "termite" "stack"; do
+for X in "castget" "irssi" "termite" "stack" "elixir"; do
   lazy_install $X
 done
