85600dd29d89771db46d69a459e7696161448f0b
tkachuk-labs <tkachuk.labs@gmail.com>
2021-02-10 23:01:30 +0200

    ssh over tor

--- install.sh
+++ install.sh
@@ -205,14 +205,62 @@ fi
   TERM=xterm-color
   log_bundle "ssh"
   sudo apt-get install -y openssh-server ufw fail2ban
-  sudo systemctl status ssh
+  sudo systemctl enable ssh
   sudo ufw default deny incoming
   sudo ufw default allow outgoing
   sudo ufw allow 22 comment 'allow SSH'
   sudo ufw enable
   sudo systemctl enable ufw
-  sudo systemctl status ufw
   sudo ufw status
+)
+
+#
+# tor
+#
+#   We need to add a couple of configuration options to the
+#   default Tor configuration file.
+#
+#   $ sudo vi /etc/tor/torrc
+#
+#   At the bottom of that file, add the following:
+#
+#     HiddenServiceDir /var/lib/tor/ssh/
+#     HiddenServicePort 22
+#
+#   Save and close the file. Restart Tor with the command:
+#
+#   $ sudo systemctl restart tor
+#
+#   The restarting of Tor will generate all the necessary
+#   files within /var/lib/tor/ssh. In that directory will be
+#   the hostname you'll need to use to connect to the server
+#   from the remote client. To find out that hostname, issue
+#   the command:
+#
+#   $ sudo cat /var/lib/tor/ssh/hostname
+#
+#   Head over to your client, where you've also installed Tor.
+#   In order to connect to the server, you'll use the hostname
+#   provided by the cat command from above.
+#   So to make the connection, you'd issue the command:
+#
+#   $ sudo torify ssh USER@HOSTNAME
+#
+#   Where USER is a remote user and HOSTNAME
+#   is the hostname provided by Tor.
+#
+(
+  TERM=xterm-color
+  TOR_SSH_HOST=/var/lib/tor/ssh/hostname
+  log_bundle "tor"
+  sudo apt-get install tor -y
+  sudo systemctl enable tor
+  if sudo bash -c "[ -f "$TOR_SSH_HOST" ]"; then
+    log_success "tor ssh hostname is $(sudo cat "$TOR_SSH_HOST")"
+  else
+    log_error "FILE $TOR_SSH_HOST DOES NOT EXIST"
+    log_error "PLEASE CONFIGURE /etc/tor/torrc ACCORDING install.sh"
+  fi
 )
 
 for X in "irssi" "stack" "elixir" "docker-compose" "htop" "gcal"; do
