cfb0bd3e7832adeb7ce613480664b922b79bf98e
tkachuk-labs <tkachuk.labs@gmail.com>
2021-02-13 18:33:00 +0200

    split code into few lib-*.sh files

--- install.sh
+++ install.sh
@@ -2,80 +2,9 @@
 
 set -e
 
-TERM=xterm-color
 DOTFILES_SOURCE_DIR="$(dirname "$(readlink -m "$0")")"
-
-log () {
-  echo "$1 ==> $2"
-}
-log_error () {
-  log "error" "$1"
-}
-
-log_success () {
-  log "success" "$1"
-}
-
-log_bundle () {
-  log "bundle" "$1"
-}
-
-log_updating () {
-  log "updating" "$1"
-}
-
-log_already_exists () {
-  log "already exists" "$1"
-}
-
-log_creating () {
-  log "creating" "$1"
-}
-
-log_installing () {
-  log "installing" "$1"
-}
-
-log_already_installed () {
-  log "already installed" "$1"
-}
-
-lazy_copy () {
-  local DOTFILES_SOURCE="$DOTFILES_SOURCE_DIR/$1"
-  local DOTFILES_TARGET="$2"
-  local DOTFILES_TARGET_DIR="$(dirname "$(readlink -m "$2")")"
-  if [ -d "$DOTFILES_TARGET_DIR" ]; then
-    log_already_exists "$DOTFILES_TARGET_DIR"
-  else
-    log_creating "$DOTFILES_TARGET_DIR"
-    mkdir -p "$DOTFILES_TARGET_DIR"
-  fi
-  if [ -f "$DOTFILES_TARGET" ]; then
-    log_already_exists "$DOTFILES_TARGET"
-  else
-    log_creating "$DOTFILES_TARGET"
-    cp "$DOTFILES_SOURCE" "$DOTFILES_TARGET"
-  fi
-}
-
-lazy_install () {
-  # 1st arg = executable name (required)
-  # 2nd arg = pkg name (optional)
-  # 3rd arg = installation command (optional)
-  local PKG="$([ -z "$2" ] && echo "$1" || echo "$2")"
-  command -v "$1" > /dev/null && \
-    log_already_installed "$PKG" || \
-    strict_install "$PKG" "$3"
-}
-
-strict_install () {
-  log_installing "$1"
-  if [ -z "$2" ]; then
-    nix-env -iAP "nixpkgs.$1"
-  else
-    eval "$2"
-  fi
-}
+source "$DOTFILES_SOURCE_DIR/lib-shared.sh"
+source "$DOTFILES_SOURCE_DIR/lib-nix.sh"
 
 if [[ $UID == 0 ]]; then
   log_error "please don't run this script with sudo!"
@@ -85,10 +14,10 @@ fi
 log_updating "apt-get"
 sudo apt-get update -y
 
-log_bundle "nix"
-if ! command -v nix &> /dev/null
-then
-  (
+(
+  log_bundle "nix"
+  if ! command -v nix &> /dev/null
+  then
     log_installing "nix"
     mkdir -p ~/Downloads
     cd ~/Downloads
@@ -102,22 +31,24 @@ then
       --recv-keys B541D55301270E0BCF15CA5D8170B4726D7198DE
     gpg2 --verify ./install-nix-2.3.10.asc
     sh ./install-nix-2.3.10
-  )
-else
-  log_already_installed "nix"
-fi
+  else
+    log_already_installed "nix"
+  fi
+)
 
-log_bundle "nixpkgs"
-NIX_CHANNELS="$(nix-channel --list)"
-EXPECTED_NIX_CHANNELS="nixpkgs https://nixos.org/channels/nixos-20.09"
-if [ "$NIX_CHANNELS" != "$EXPECTED_NIX_CHANNELS" ]; then
-  log_error "got '$NIX_CHANNELS' instead of '$EXPECTED_NIX_CHANNELS'"
-  log_installing "nixpkgs"
-  nix-channel --add https://nixos.org/channels/nixos-20.09 nixpkgs
-  nix-channel --update
-else
-  log_already_installed "nixpkgs"
-fi
+(
+  log_bundle "nixpkgs"
+  NIX_CHANNELS="$(nix-channel --list)"
+  EXPECTED_NIX_CHANNELS="nixpkgs https://nixos.org/channels/nixos-20.09"
+  if [ "$NIX_CHANNELS" != "$EXPECTED_NIX_CHANNELS" ]; then
+    log_error "got '$NIX_CHANNELS' instead of '$EXPECTED_NIX_CHANNELS'"
+    log_installing "nixpkgs"
+    nix-channel --add https://nixos.org/channels/nixos-20.09 nixpkgs
+    nix-channel --update
+  else
+    log_already_installed "nixpkgs"
+  fi
+)
 
 (
   log_bundle "vim"
@@ -291,7 +222,7 @@ fi
     log_success "tor ssh hostname is $(sudo cat "$TOR_SSH_HOST")"
   else
     log_error "FILE $TOR_SSH_HOST DOES NOT EXIST"
-    log_error "PLEASE FIX install.sh OR /etc/tor/torrc"
+    log_error "PLEASE FIX /etc/tor/torrc"
   fi
 )
 
---
+++ lib-shared.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -e
+
+TERM=xterm-color
+DOTFILES_SOURCE_DIR="$(dirname "$(readlink -m "$0")")"
+
+log () {
+  echo "$1 ==> $2"
+}
+log_error () {
+  log "error" "$1"
+}
+
+log_success () {
+  log "success" "$1"
+}
+
+log_bundle () {
+  log "bundle" "$1"
+}
+
+log_updating () {
+  log "updating" "$1"
+}
+
+log_already_exists () {
+  log "already exists" "$1"
+}
+
+log_creating () {
+  log "creating" "$1"
+}
+
+log_installing () {
+  log "installing" "$1"
+}
+
+log_already_installed () {
+  log "already installed" "$1"
+}
+
+lazy_copy () {
+  local DOTFILES_SOURCE="$DOTFILES_SOURCE_DIR/$1"
+  local DOTFILES_TARGET="$2"
+  local DOTFILES_TARGET_DIR="$(dirname "$(readlink -m "$2")")"
+  if [ -d "$DOTFILES_TARGET_DIR" ]; then
+    log_already_exists "$DOTFILES_TARGET_DIR"
+  else
+    log_creating "$DOTFILES_TARGET_DIR"
+    mkdir -p "$DOTFILES_TARGET_DIR"
+  fi
+  if [ -f "$DOTFILES_TARGET" ]; then
+    log_already_exists "$DOTFILES_TARGET"
+  else
+    log_creating "$DOTFILES_TARGET"
+    cp "$DOTFILES_SOURCE" "$DOTFILES_TARGET"
+  fi
+}
