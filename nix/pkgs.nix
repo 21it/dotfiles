bc974472ffadaf579eb484efe60fea501e1797f1
tkachuk-labs <tkachuk.labs@gmail.com>
2022-01-25 16:52:29 +0200

    expect

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -8,5 +8,6 @@ with pkgs;
   inherit xkbset xdotool;
   inherit ormolu hpack;
   inherit patchelf;
+  inherit expect;
   #tok = import (fetchTarball "https://github.com/21it/tok/tarball/848b16af983ba9687501a3864eef869ef70e9615");
 }
