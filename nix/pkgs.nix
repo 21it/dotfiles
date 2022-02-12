ac64d99c32f94f96a904962fb2373861a50616d7
tkachuk-labs <tkachuk.labs@gmail.com>
2022-02-12 19:56:17 +0200

    feh and imagemagic

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -9,5 +9,6 @@ with pkgs;
   inherit ormolu hpack;
   inherit patchelf;
   inherit expect;
+  inherit feh imlib2 imagemagick;
   #tok = import (fetchTarball "https://github.com/21it/tok/tarball/848b16af983ba9687501a3864eef869ef70e9615");
 }
