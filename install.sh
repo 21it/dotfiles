3f9e322dbcbfb892683202dc7efedde02a564af7
tkachuk-labs <tkachuk.labs@gmail.com>
2022-03-19 22:51:06 +0200

    upgrade nixpkgs, add spanish-podcast, add signal and htmldoc

--- install.sh
+++ install.sh
@@ -39,11 +39,11 @@ sudo apt-get update -y
 (
   log_bundle "nixpkgs"
   NIX_CHANNELS="$(nix-channel --list)"
-  EXPECTED_NIX_CHANNELS=$'nixpkgs https://nixos.org/channels/nixos-21.05\nnixpkgs-unstable https://nixos.org/channels/nixos-unstable'
+  EXPECTED_NIX_CHANNELS=$'nixpkgs https://nixos.org/channels/nixos-21.11\nnixpkgs-unstable https://nixos.org/channels/nixos-unstable'
   if [ "$NIX_CHANNELS" != "$EXPECTED_NIX_CHANNELS" ]; then
     log_error "got '$NIX_CHANNELS' instead of '$EXPECTED_NIX_CHANNELS'"
     log_installing "nixpkgs"
-    nix-channel --add https://nixos.org/channels/nixos-21.05 nixpkgs
+    nix-channel --add https://nixos.org/channels/nixos-21.11 nixpkgs
     nix-channel --add https://nixos.org/channels/nixos-unstable nixpkgs-unstable
   else
     log_already_installed "nixpkgs"
@@ -126,6 +126,7 @@ sudo apt-get update -y
   mkdir -p ~/podcast/the-haskell-cast
   mkdir -p ~/podcast/compositional
   mkdir -p ~/podcast/tales-from-the-crypt
+  mkdir -p ~/podcast/spanish-podcast
   lazy_install "castget"
 )
 
--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -3,12 +3,13 @@
 }:
 with pkgs;
 {
-  inherit irssi slack skypeforlinux;
+  inherit irssi slack skypeforlinux signal-desktop;
   inherit yarn;
   inherit xkbset xdotool;
   inherit ormolu hpack;
   inherit patchelf;
   inherit expect;
   inherit feh imlib2 imagemagick;
+  inherit htmldoc;
   #tok = import (fetchTarball "https://github.com/21it/tok/tarball/848b16af983ba9687501a3864eef869ef70e9615");
 }
